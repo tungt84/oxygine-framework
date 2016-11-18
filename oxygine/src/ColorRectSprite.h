22a69cb1c77aefbe64c77ba530b647d18bc7a46c
dmuratshin <frankinshtein85@gmail.com>
2016-11-18 05:06:31 +0500

    min

--- oxygine/src/ColorRectSprite.h
+++ oxygine/src/ColorRectSprite.h
@@ -18,11 +18,6 @@ namespace oxygine
         void deserialize(const deserializedata* data);
 
         void doRender(const RenderState& rs);
-
-        RectF getDestRect() const OVERRIDE { return Actor::getDestRect(); }
-
-    protected:
-        void sizeChanged(const Vector2& size) OVERRIDE;
     };
 }
 
